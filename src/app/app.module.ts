997fad7c8da3c8f6aa019335224417efbfe92ca3
DESKTOP-I7NIUE3\stroj <jakubstr@student.agh.edu.pl>
2022-03-12 11:43:15 +0100

    appmodule modification and assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,6 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 import { AppComponent } from './app.component';
-import { Lab4Zad7Component } from './components/old Components/lab4-zad7/lab4-zad7.component';
-import { Lab4Zad6ParentComponent } from './components/old Components/lab4-zad6-parent/lab4-zad6-parent.component';
-import { Lab4Zad6ChildComponent } from './components/old Components/lab4-zad6-child/lab4-zad6-child.component';
-import { Lab4Zad5AComponent } from './components/old Components/lab4-zad5-a/lab4-zad5-a.component';
 import { HeaderComponent } from './components/restauracja-header/header.component';
 import { RestauracjaAddBtnComponent } from './components/restauracja-add-btn/restauracja-add-btn.component';
 import { RestauracjaDishesComponent } from './components/restauracja-dishes/restauracja-dishes.component';
@@ -22,11 +18,6 @@ import { RestauracjaDishItemComponent } from './components/restauracja-dish-item
 import { RestauracjaContainerComponent } from './components/restauracja-container/restauracja-container.component';
 import { RestauracjaFormComponent } from './components/restauracja-form/restauracja-form.component';
 import { RestauracjaCartComponent } from './components/restauracja-cart/restauracja-cart.component';
-import { Lab5Zad1PostsComponent } from './components/old Components/lab5-zad1-posts/lab5-zad1-posts.component';
-import { Lab5Zad1PhotosComponent } from './components/old Components/lab5-zad1-photos/lab5-zad1-photos.component';
-import { Lab5Zad1HomeComponent } from './components/old Components/lab5-zad1-home/lab5-zad1-home.component';
-import { Lab5Zad1HeaderComponent } from './components/old Components/lab5-zad1-header/lab5-zad1-header.component';
-import { Lab5Zad1OnephotoComponent } from './components/old Components/lab5-zad1-onephoto/lab5-zad1-onephoto.component';
 import { RestauracjaInfoComponent } from './components/restauracja-info/restauracja-info.component';
 import { RestauracjaDetailViewComponent } from './components/restauracja-detail-view/restauracja-detail-view.component';
 import { LoginpanelComponent } from './components/loginpanel/loginpanel.component';
@@ -36,10 +27,6 @@ import { RestauracjaDishItemSpecialComponent } from './components/restauracja-di
 @NgModule({
   declarations: [
     AppComponent,
-    Lab4Zad7Component,
-    Lab4Zad6ParentComponent,
-    Lab4Zad6ChildComponent,
-    Lab4Zad5AComponent,
     HeaderComponent,
     RestauracjaAddBtnComponent,
     RestauracjaDishesComponent,
@@ -47,11 +34,6 @@ import { RestauracjaDishItemSpecialComponent } from './components/restauracja-di
     RestauracjaContainerComponent,
     RestauracjaFormComponent,
     RestauracjaCartComponent,
-    Lab5Zad1PostsComponent,
-    Lab5Zad1PhotosComponent,
-    Lab5Zad1HomeComponent,
-    Lab5Zad1HeaderComponent,
-    Lab5Zad1OnephotoComponent,
     RestauracjaInfoComponent,
     RestauracjaDetailViewComponent,
     LoginpanelComponent,
